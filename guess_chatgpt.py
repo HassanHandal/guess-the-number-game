f600881b6329e896a64b88348b78a9144a09431c
HassanHandal <hassanhamdyfarid@gmail.com>
2023-11-25 22:50:12 +0300

    chat gpt new version

--- guess_chatgpt.py
+++ guess_chatgpt.py
@@ -1,30 +1,32 @@
 import random
+from check_integer import check_int
 
 user_name = input("Hello, let's play a guessing game. What's your name?:\n")
 attempts = []
 
 while True:
-    print(f"There {'is no high score yet' if not attempts else f'is currently a high score of {min(attempts)}. Try to break it.'}")
-
-    try:
-        user_choice = int(input(f"{user_name}, guess the number I have chosen. It must be from 1 to 10:\n"))
-    except ValueError:
-        print("Error: Please enter a valid number.")
-        continue
-
-    pc_random = random.randint(1, 10)
+    pc_random = str(random.randint(1, 10))
+    user_choice = check_int(input(f"{user_name}, guess the number I've chosen between 1 and 10:\n"))
+    
     attemp_no = 1
 
     while user_choice != pc_random:
         attemp_no += 1
-        user_choice = int(input(f"No {user_name}, it is {'smaller' if user_choice > pc_random else 'greater'} than your guess. Enter a {'smaller' if user_choice > pc_random else 'greater'} number:\n"))
+        user_choice = check_int(input(f"No {user_name}, it's {'smaller' if user_choice > pc_random else 'greater'} than your choice. Guess again:\n"))
 
     attempts.append(attemp_no)
-    print(f"Eureka! You guessed the right number, {user_name}, and you used {attemp_no} attempts.")
+    print(f"Eureka, you guessed the right number, {user_name}, in {attemp_no} attempts")
 
-    ask = input("Do you want to play again? (Y/N):\n").lower()
+    ask = input("Do you want to play again? Y/N:\n").lower()
+    
     while ask not in ["n", "y"]:
-        ask = input("Enter an available choice (Y/N):\n").lower()
+        ask = input("Enter an available choice:\n")
 
     if ask == "n":
         break
+
+if not attempts:
+    print("Thanks for playing!")
+else:
+    high_score = min(attempts)
+    print(f"The current high score is {high_score}. Thanks for playing!")
